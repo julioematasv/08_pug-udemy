af6ff875f065dac18080e9c7edb3edb032272c03
Julio Mata <julioematasv@gmail.com>
2023-01-11 09:37:47 +0100

    Añadiendo los enlaces a archivos Javascript, en este caso main.js

---
+++ VictorRobles2022/public/js/main.js
@@ -0,0 +1,9 @@
+
+function miFuncion1(){
+    alert("Hola, la función esta ejecutandose bien");
+}
+
+function miFuncion2(){
+    let datos = prompt("Hola, Introduce tu nombre", "");
+    document.write("Hola, " + datos);
+}
